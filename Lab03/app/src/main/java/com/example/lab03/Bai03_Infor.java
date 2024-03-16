cb9678b1e12e8257f4baef49ea7f79e8f1cde234
hadeszhangg <truongdangminhtan333@gmail.com>
2024-03-16 14:13:17 +0700

    Upload new source

---
+++ Lab03/app/src/main/java/com/example/lab03/Bai03_Infor.java
@@ -0,0 +1,14 @@
+package com.example.lab03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bai03_Infor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai03_infor);
+    }
+}
